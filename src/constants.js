ef023c779f0c72dbc82c279733bf8cfe27157c9d
Ross O'Reilly <oreillyross@gmail.com>
2017-10-18 21:48:26 +0000

    add server for local testing

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-export const API_SERVER =
-  "https://pantlerv3-oreillyross.c9users.io/api/ingredients/"
+const C9Server =  "https://pantlerv3-oreillyross.c9users.io"
+const LocalHost =  "https://localhost:8080"
+const EndPoint = "/api/ingredients/"
+
+
+export const API_SERVER = LocalHost + EndPoint
