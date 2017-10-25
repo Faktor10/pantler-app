b1a90335500aa6baeaa38cd2074dea5dd69928c6
Ross O'Reilly <oreillyross@gmail.com>
2017-10-25 09:47:42 +0000

    dummy test for ci

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ import "./css/App.css";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <div>
-          <Route exact path="/" component={IngredientDashboard} />
-          <Route path="/gridview" component={GridIngredientDashboard} />
-        </div>/>
+      <div className="container">
+        <div className="header">Pantler</div>
+        <div className="content">
+          <div className="content--subheader"> Sub header </div>
+          <div className="content--ingredientgrid">
+            <div className="ingredient">1</div>
+            <div className="ingredient">2</div>
+            <div className="ingredient">3</div>
+            <div className="ingredient">4</div>
+            <div className="ingredient">5</div>
+            <div className="ingredient">6</div>
+          </div>
+        </div>
+        <div className="whitespace--left">Left</div>
+        <div className="whitespace--right">Right</div>
+        <div className="footer">Footer</div>
       </div>
     );
   }
